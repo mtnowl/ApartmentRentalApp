cfbec4b56b9569994b383174bb4b8890b5f7ece4
Henry <henry@thejins.org>
2020-05-18 16:49:01 -0500

    Initial add

---
+++ ApartmentRental/Models/Apartment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApartmentRental.Models
+{
+    public class Apartment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Area { get; set; }
+        public decimal MonthlyPrice { get; set; }
+        public int Rooms { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public DateTime DateAdded { get; set; }
+        //public Realtor AssignedRealtor { get; set; }
+
+    }
+}
---
+++ ApartmentRental/Models/ApartmentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApartmentRental.Models
+{
+    public class ApartmentContext : DbContext
+    {
+        public ApartmentContext(DbContextOptions<ApartmentContext> options)
+            :base(options) 
+        { 
+        }
+
+        public DbSet<Apartment> Apartments { get; set; }
+    }
+}
