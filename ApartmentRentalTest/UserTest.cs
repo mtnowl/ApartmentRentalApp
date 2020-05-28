676ee88135ac0a2d1d89581492b499e101cad24c
Henry <henry@thejins.org>
2020-05-28 18:47:51 -0500

    fix usertest to use password salts

--- ApartmentRentalTest/UserTest.cs
+++ ApartmentRentalTest/UserTest.cs
@@ -70,9 +70,24 @@ namespace ApartmentRentalTest
                 .Options;
             using (var context = new ApartmentContext(options))
             {
-                context.Users.Add(new User { Id = 1, Username = "admin", Password = "admin", Role = Role.Admin });
-                context.Users.Add(new User { Id = 2, Username = "realtor", Password = "realtor", Role = Role.Realtor });
-                context.Users.Add(new User { Id = 3, Username = "client", Password = "client", Role = Role.Client });
+                context.Users.Add(new User { 
+                    Id = 1, 
+                    Username = "admin", 
+                    Password = "OT672nTS108fBgHT9JWFEucWAv+dGEiS0X5E2G65TWI=", 
+                    PasswordSalt = Convert.FromBase64String("SEfcsflTemzwn2t/MP00Mw=="), 
+                    Role = Role.Admin });
+                context.Users.Add(new User { 
+                    Id = 2, 
+                    Username = "realtor", 
+                    Password = "1zjqPT0Db2SkhOS4OTS2/37QRQwe/ezFkDN+D47QIO4=", 
+                    PasswordSalt = Convert.FromBase64String("tYwvfcbNoAFjLOgwhYpGaQ=="), 
+                    Role = Role.Realtor });
+                context.Users.Add(new User { 
+                    Id = 3, 
+                    Username = "client", 
+                    Password = "0BBnc7tkaLphnwl207AJTLjQwrzV/sFftfHVvTv8GPw=", 
+                    PasswordSalt = Convert.FromBase64String("BAV88E56WMdNcAU5y7ZDfw=="), 
+                    Role = Role.Client });
                 context.SaveChanges();
             }
 
